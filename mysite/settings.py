a6d1e38830ba1ad09ca5694f17a67420374fdab4
nicksonlangat <nicksonlangat95@gmail.com>
2021-06-07 10:57:39 +0300

    added projects

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# if DEBUG:
+#     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # else:
     
 
