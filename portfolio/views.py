d88c0a2885944221865933e2bb4bb65c19dd7acc
nicksonlangat <nicksonlangat95@gmail.com>
2021-06-07 12:03:10 +0300

    fixes

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from .models import Project
 # Create your views here.
 
 def portal_index(request):
+    projects=Project.objects.all()
     context={
-        
+        'projects':projects,
     }
     return render(request, 'portfolio/index.html', context)
