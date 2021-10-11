230382184da33338a287a1fc5c550a3b5d6b5a3c
nicksonlangat <nicksonlangat95@gmail.com>
2021-10-11 17:29:55 +0300

    fixes

--- locations/models.py
+++ locations/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class County(models.Model):
-    name=models.CharField(max_length=250)
+    name=models.CharField(max_length=250, unique=True)
     date=models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
@@ -11,7 +11,7 @@ class County(models.Model):
 
 class Constituency(models.Model):
     county=models.ForeignKey(County, on_delete=models.CASCADE)
-    name=models.CharField(max_length=250)
+    name=models.CharField(max_length=250, unique=True)
     date=models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
@@ -19,7 +19,7 @@ class Constituency(models.Model):
 
 class Ward(models.Model):
     constituency=models.ForeignKey(Constituency, on_delete=models.CASCADE)
-    name=models.CharField(max_length=250)
+    name=models.CharField(max_length=250, unique=True)
     date=models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
