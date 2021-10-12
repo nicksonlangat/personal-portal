f221dac9043240cbebcc23e32a83fea4515f382b
nicksonlangat <nicksonlangat95@gmail.com>
2021-10-12 11:07:52 +0300

    fixes

--- mysite/settings.py
+++ mysite/settings.py
@@ -146,8 +146,8 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.User'
 
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+#     'PAGE_SIZE': 20
+# }
 
